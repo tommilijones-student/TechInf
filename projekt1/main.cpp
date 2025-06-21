19345d87f25c8a4e17291461d285d5765d8c8b4d
Tom <11tommilijones11@gmail.com>
2025-06-21 23:40:01 +0200

    poprawka mediany

--- projekt1/main.cpp
+++ projekt1/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <cstdio>
 
-void druk(int s[]) {
+void druk(int s[], int size) {
     printf("tablica:\n");
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < size; i++) {
         printf("%d\n", s[i]);
     }
 }
 
-void sortuj(int s[], int rozmiar) {
-    for (int i = 0; i < rozmiar - 1; i++) {
-        for (int j = i + 1; j < rozmiar; j++) {
+void sortuj(int s[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
             if (s[i] > s[j]) {
                 int temp = s[i];
                 s[i] = s[j];
@@ -20,28 +20,37 @@ void sortuj(int s[], int rozmiar) {
     }
 }
 
+double liczMediane(int s[], int size) {
+    if (size % 2 == 1) {
+
+        return s[size / 2];
+    } else {
+
+        return (s[size / 2 - 1] + s[size / 2]) / 2.0;
+    }
+}
+
 int main() {
-    int number[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    druk(number);
+    const int SIZE = 10;
+    int number[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    druk(number, SIZE);
 
     int maks = number[0], min = number[0];
 
-    for (int i = 1; i < 10; i++) {
-        if (number[i] > maks) {
-            maks = number[i];
-        }
-        if (number[i] < min) {
-            min = number[i];
-        }
+    for (int i = 1; i < SIZE; i++) {
+        if (number[i] > maks) maks = number[i];
+        if (number[i] < min) min = number[i];
     }
 
     printf("\nNajwieksza wartosc to: %d\n", maks);
     printf("Najmniejsza wartosc to: %d\n\n", min);
 
-    sortuj(number, 10);
+    sortuj(number, SIZE);
+    druk(number, SIZE);
 
-
-    printf("mediana=%d\n", number[10/2]);
+    double mediana = liczMediane(number, SIZE);
+    printf("\nMediana = %.1f\n", mediana);
 
     return 0;
 }
