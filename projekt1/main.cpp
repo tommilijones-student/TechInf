61ab4b169f55d822ccfa27834217ebb7254b3531
Tom <11tommilijones11@gmail.com>
2025-06-21 23:54:44 +0200

    kolejna wersja

--- projekt1/main.cpp
+++ projekt1/main.cpp
@@ -1,32 +1,15 @@
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
+
+int porownaj(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
 
 void druk(int s[], int size) {
     printf("tablica:\n");
     for (int i = 0; i < size; i++) {
         printf("%d\n", s[i]);
-    }
-}
-
-void sortuj(int s[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (s[i] > s[j]) {
-                int temp = s[i];
-                s[i] = s[j];
-                s[j] = temp;
-            }
-        }
-    }
-}
-
-double liczMediane(int s[], int size) {
-    if (size % 2 == 1) {
-
-        return s[size / 2];
-    } else {
-
-        return (s[size / 2 - 1] + s[size / 2]) / 2.0;
     }
 }
 
@@ -39,18 +22,21 @@ int main() {
     int maks = number[0], min = number[0];
 
     for (int i = 1; i < SIZE; i++) {
-        if (number[i] > maks) maks = number[i];
-        if (number[i] < min) min = number[i];
+        if (number[i] > maks) {
+            maks = number[i];
+        }
+        if (number[i] < min) {
+            min = number[i];
+        }
     }
 
     printf("\nNajwieksza wartosc to: %d\n", maks);
     printf("Najmniejsza wartosc to: %d\n\n", min);
 
-    sortuj(number, SIZE);
+    qsort(number, SIZE, sizeof(int), porownaj);
     druk(number, SIZE);
 
-    double mediana = liczMediane(number, SIZE);
-    printf("\nMediana = %.1f\n", mediana);
+    printf("\nMediana = %d\n", number[SIZE / 2]);
 
     return 0;
 }
